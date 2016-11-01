1df61853717f0aa08c99f1184afe10a094928113
grzhan <envy518@gmail.com>
2016-11-02 01:24:05 +0800

    chore: introduce dotenv module(load env-var into process.env)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	watch = require('gulp-watch');
-var destDir = '/Users/ivan_l/Library/Application\ Support/poi/plugins/node_modules/poi-plugin-kcwiki-report';
+require('dotenv').config()
+var destDir = process.env.PLUGIN_RUN_DIR
 
 // Sync files between git repo and local poi plugin directory
 gulp.task('watch', function() {
